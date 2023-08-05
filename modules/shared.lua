6242557e7f37aa76fc215b1db3e3cd292dec961c
xThrasherrr <101474430+xThrasherrr@users.noreply.github.com>
2023-08-04 22:53:08 -0600

    chore(modules): remove unused shared module

--- modules/shared.lua
+++ modules/shared.lua
@@ -15,41 +15,4 @@ function Utils.Debug(type, debugTxt)
     end
 end
 
--- Check Job Client/Server --
-function Utils.CheckJob(job)
-    local checkType = IsDuplicityVersion() and 'server' or 'client'
-    local callback = false
-    if checkType == 'client' then -- Checks for client/server
-        local Player = QBCore.Functions.GetPlayerData()
-        if type(job) == 'string' then -- Checks for string or table of jobs
-            if Player.job.name == job then
-                callback = true
-            end
-        elseif type(job) == 'table' then
-            for _,v in pairs(job) do
-                if Player.job.name == v then
-                    callback = true
-                    break
-                end
-            end
-        end
-    else
-        local Player = QBCore.Functions.GetPlayer(source)
-        if type(job) == 'string' then
-            if Player.PlayerData.job.name == job then
-                callback = true
-            end
-        elseif type(job) == 'table' then
-            for _,v in pairs(job) do
-                if Player.PlayerData.job.name == v then
-                    callback = true
-                    break
-                end
-            end
-        end
-    end
-    Utils.Debug('Job Check', 'Type: '..checkType)
-    return callback
-end
-
 return Utils
