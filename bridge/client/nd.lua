3d0e82b2b33df49c47807144dbcc736f9a3a5459
xThrasherrr <thrashergamezz@gmail.com>
2024-08-03 00:59:39 -0600

    fix: ND handlers

--- bridge/client/nd.lua
+++ bridge/client/nd.lua
@@ -1,14 +1,9 @@
 if not lib.checkDependency('ND_Core', '2.0.0') then return end
 
---[[
-    TO-DO:
-    - Add compat events for jailing (if there is any)
-]]
-
-AddEventHandler('ND:characterLoaded', function()
+RegisterNetEvent('ND:characterLoaded', function()
     TriggerEvent('xt-prison:client:onLoad')
 end)
 
-AddEventHandler('ND:characterUnloaded', function()
+RegisterNetEvent('ND:characterUnloaded', function()
     TriggerEvent('xt-prison:client:onUnload')
 end)
