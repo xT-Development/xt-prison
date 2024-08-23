c20cdbf9f2f04ed2df3d8973b71f8859fc8e5282
xThrasherrr <thrashergamezz@gmail.com>
2024-08-22 20:32:35 -0600

    tweak(qbx): require qbx v1.18.0 for HasGroup

--- bridge/client/qbx.lua
+++ bridge/client/qbx.lua
@@ -1,4 +1,6 @@
-if GetResourceState('qbx_core') ~= 'started' then return end
+if GetResourceState('qbx_core') == 'started' and not lib.checkDependency('qbx_core', '1.18.0') then
+    return lib.print.error('qbx_core v1.18.0 is required for xt-prison') -- Requires 1.18.0 for HasGroup export || https://github.com/Qbox-project/qbx_core/blob/c6f2b96a3644edce5958c76b447a98afa4801475/server/functions.lua#L447
+end
 
 -- Load / Unload Events --
 RegisterNetEvent('QBCore:Client:OnPlayerLoaded', function()
